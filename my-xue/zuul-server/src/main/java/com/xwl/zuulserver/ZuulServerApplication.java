d3f8677a18ef1642e6fd8f1b272b07aa87725849
xwlgithub <17530235750@163.com>
2020-06-01 16:40:35 +0800

    整合依赖

--- my-xue/zuul-server/src/main/java/com/xwl/zuulserver/ZuulServerApplication.java
+++ my-xue/zuul-server/src/main/java/com/xwl/zuulserver/ZuulServerApplication.java
@@ -3,13 +3,18 @@ package com.xwl.zuulserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * 网关注入注册中心
  */
+//Boot启动必须类
 @SpringBootApplication
-@EnableZuulServer
+//开启网关转发服务
+@EnableZuulProxy
+//被注册中心发现
+@EnableEurekaClient
 @MapperScan("com.xwl.zuulserver.mapper")
 public class ZuulServerApplication {
 
